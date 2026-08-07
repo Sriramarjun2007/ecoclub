591bd3c33e1d781da7f4247b8cc3aee3c5c74278
Sriram Arjun <sriramarjun2007@gmail.com>
2026-08-08 00:22:14 +0530

    Add API health endpoint

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,13 +1,31 @@
 """Root URL configuration for the ECO CLUB platform."""
+
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.http import JsonResponse
 from django.urls import include, path
 
+
+def home(request):
+    return JsonResponse({
+        "status": "success",
+        "message": "ECO CLUB API is running!",
+        "version": "1.0",
+    })
+
+
 urlpatterns = [
+    path("", home, name="home"),
+
     path("django-admin/", admin.site.urls),
+
     path("api/", include("config.api_urls")),
 ]
 
+
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
